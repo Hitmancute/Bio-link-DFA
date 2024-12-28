c197c32906c4c01c740b217a4b3a97b7013cdf70
Hitmancute <fuadmatang1@gmail.com>
2024-12-28 13:46:59 +0700

    update 2.0

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+//scrip untuk membuat link bisa di copy
 const actionLink = document.querySelectorAll(".link-card .link-action");
 
 actionLink.forEach((action) => {
   action.addEventListener("click", (e) => {
     e.preventDefault();
     navigator.clipboard.writeText(action.parentElement.getAttribute("href"));
+    //untuk memunculkan toast notification
     document.getElementById("toast").innerHTML = `
         <div class="toast-container">
-            <p>✅ Link Berhasil disalin</p>
+            <p>✅ Link  <strong>${action.parentElement.innerText}</strong> Berhasil disalin</p>
         </div>
     `;
 
+    //untuk menghilangkan toast notification
     setTimeout(() => {
       document
         .querySelector("#toast .toast-container")
@@ -20,4 +23,25 @@ actionLink.forEach((action) => {
       document.querySelector("#toast .toast-container").remove();
     }, 2000);
   });
+});
+
+//untuk ganti icon sosmed saat hover
+document.querySelectorAll(".sosmed i").forEach((sosmed) => {
+  sosmed.addEventListener("mouseenter", () => {
+    sosmed.classList.remove("ph");
+    sosmed.classList.add("ph-fill");
+  });
+
+  sosmed.addEventListener("mouseleave", () => {
+    sosmed.classList.remove("ph-fill");
+    sosmed.classList.add("ph");
+  });
+});
+
+//animasi text bergerak scroll
+// animasi text bergerak saat di scroll
+document.addEventListener("scroll", (e) => {
+  document.querySelector(".bg-text-animation").style.transform = `translateX(${
+    window.scrollY / 5
+  }px)`;
 });
